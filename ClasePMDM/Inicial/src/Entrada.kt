0870efadd7dded0e07de9e7222b12f7ec549191c
jsanchoc <jsanchocalzada@gmail.com>
2025-09-11 15:56:58 +0200

    Creación repositorio y primer proyecto

---
+++ ClasePMDM/Inicial/src/Entrada.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>){
+
+    val DNI = "12332145E"
+    var nombre: String = "Jorge"
+    var edad: Number = 41;
+    lateinit var correo: String
+
+
+    println("Primer programa de repaso kotlin")
+    correo = "jorge.sancho@gmail.com"
+    println("Hola mi nombre es $nombre, tengo $edad y mi correo es $correo")
+
+
+}
