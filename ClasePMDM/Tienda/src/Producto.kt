cb0c25fd2adbeb109a22b3983cbe2eed2dd0bf14
jsanchoc <jsanchocalzada@gmail.com>
2025-09-23 16:18:35 +0200

    ??

--- ClasePMDM/Tienda/src/Producto.kt
+++ ClasePMDM/Tienda/src/Producto.kt
@@ -1,2 +1,13 @@
-class Producto {
+
+class Producto( var id: Int?=null,
+                var precio: Double?=null,
+                var nombre: String?=null,
+                var descripcion:String?=null) {
+
+    fun mostrarDatos() {
+        println("ID: ${id ?: "Sin definir"}")
+        println("Precio: ${precio ?: "Sin definir"}")
+        println("Nombre: ${nombre ?: "Sin definir"}")
+        println("Descripcion: ${descripcion ?: "Sin definir"}")
+    }
 }
